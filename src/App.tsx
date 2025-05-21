bc0274c8f2b1563c0b7b259af0abb349ad403de5
John Szymanowski <github@john.szymanow.ski>
2025-05-21 22:21:27 +0800

    Add branch assets (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { Container, Text } from "@still-forest/canopy";
-import Layout from "./components/Layout";
+import { Container, Flex, Text } from "@still-forest/canopy";
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
     <Layout>
-      <Container>
-        <Text>Hi there.</Text>
+      <Container className="h-full">
+        <Flex justify="between" direction="col" gap="4" className="h-full">
+          <Flex.Item flex="auto" className="flex items-center justify-center">
+            <img src="/logo-full-lg.png" alt="Logo" className="w-xl" />
+          </Flex.Item>
+          <Flex.Item flex="auto" className="flex items-center justify-center">
+            <Text className="text-center">Coming soon.</Text>
+          </Flex.Item>
+        </Flex>
       </Container>
     </Layout>
   );
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,27 @@
 import { Container, Flex, Footer, Text } from "@still-forest/canopy";
-import { Header } from "./Header";
+import { GITHUB_URL, LINKEDIN_URL } from "@/config";
+import { ReactComponent as GithubIcon } from "../assets/github.svg";
+import { ReactComponent as LinkedInIcon } from "../assets/linkedin.svg";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Flex direction="col" className="mx-auto h-full w-full max-w-screen-lg">
-      <Header />
-      <Container as="main" className="flex-1">
+    <Flex direction="col" className="mx-auto h-screen w-full max-w-screen-lg">
+      <Container as="main" className="mx-0 flex-1">
         {children}
       </Container>
-      <Footer>
+      <Footer className="flex items-center justify-between">
         <Text variant="muted" size="sm">
           © 2025 Still Forest LLC.
         </Text>
+        <Flex gap="4">
+          <a href={GITHUB_URL} target="_blank" rel="noopener noreferrer">
+            <GithubIcon width={32} height={32} className="text-primary/60 hover:text-primary" />
+          </a>
+          <a href={LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
+            <LinkedInIcon width={32} height={32} className="text-primary/60 hover:text-primary" />
+          </a>
+        </Flex>
       </Footer>
     </Flex>
   );
-}
+};
