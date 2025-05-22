1e510e007a70d69adcdf1aeac271ee4e378b53dd
John Szymanowski <github@john.szymanow.ski>
2025-05-22 15:36:01 +0800

    basic test

--- tests/components/ContactForm.test.tsx
+++ tests/components/ContactForm.test.tsx
@@ -1,11 +1,24 @@
-import { render } from "@testing-library/react";
-import { describe } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
 import "@testing-library/jest-dom";
 
 import { ContactForm } from "@/components/ContactForm/ContactForm";
 
 describe("ContactForm", () => {
-  it("should render", () => {
+  test("should render with all inputs", () => {
     render(<ContactForm />);
+
+    const openButton = screen.getByRole("button", { name: "Get in touch" });
+    expect(openButton).toBeVisible();
+
+    fireEvent.click(openButton);
+
+    expect(openButton).not.toBeVisible();
+
+    expect(screen.getByLabelText("Email")).toBeInTheDocument();
+    expect(screen.getByLabelText("Message")).toBeInTheDocument();
+
+    expect(screen.getByRole("button", { name: "Send" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Cancel" })).toBeInTheDocument();
   });
 });
