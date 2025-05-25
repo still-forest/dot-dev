4b1c213e548850e11cc2af374b7d9a551c38c373
John Szymanowski <github@john.szymanow.ski>
2025-05-25 20:42:57 +0800

    get web sorted out

--- apps/web/src/components/ContactForm/ContactForm.tsx
+++ apps/web/src/components/ContactForm/ContactForm.tsx
@@ -28,7 +28,12 @@ const Form = ({ onSubmit, onCancel, submitting }: FormProps) => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <Flex direction="row" gap="2">
         <Flex.Item className="flex min-w-[220px] flex-col gap-y-2">
-          <TextInput placeholder="your@email.com" aria-label="Email" {...register("email")} className="bg-input" />
+          <TextInput
+            placeholder="your@email.com"
+            aria-label="Email"
+            {...register("email")}
+            className="bg-input"
+          />
           {errors.email && <InputError message={errors.email.message!} />}
         </Flex.Item>
         <Flex.Item flex="1" className="flex flex-col gap-y-2">
@@ -42,7 +47,12 @@ const Form = ({ onSubmit, onCancel, submitting }: FormProps) => {
         </Flex.Item>
         <Flex justify="end" gap="2">
           <SubmitButton disabled={!isValid} submitting={submitting} />
-          <Button variant="secondary" onClick={onCancel} icon={<CircleX />} aria-label="Cancel" />
+          <Button
+            variant="secondary"
+            onClick={onCancel}
+            icon={<CircleX />}
+            aria-label="Cancel"
+          />
         </Flex>
       </Flex>
     </form>
@@ -60,11 +70,10 @@ export const ContactForm = () => {
     setIsSubmitting(true);
     setSubmitError(null);
 
-    console.log("data", data);
-    console.log("canExecute", canExecute());
-
     if (!canExecute()) {
-      setSubmitError("You are sending messages too quickly. Please try again later.");
+      setSubmitError(
+        "You are sending messages too quickly. Please try again later."
+      );
       setIsSubmitting(false);
       return;
     }
@@ -75,7 +84,9 @@ export const ContactForm = () => {
         setIsOpen(false);
         setHasSubmitted(true);
       } catch (error) {
-        setSubmitError(error instanceof Error ? error.message : "Failed to submit form");
+        setSubmitError(
+          error instanceof Error ? error.message : "Failed to submit form"
+        );
       } finally {
         setIsSubmitting(false);
       }
@@ -84,18 +95,32 @@ export const ContactForm = () => {
 
   return (
     <Flex direction="col" gap="2" className="w-full" data-testid="contact-form">
-      {submitError && <Alert message={submitError} title="Submission failure" type="error" />}
+      {submitError && (
+        <Alert message={submitError} title="Submission failure" type="error" />
+      )}
       {!isOpen && !hasSubmitted && (
         <Flex justify="center">
-          {canExecute() ? <Button onClick={() => setIsOpen(true)}>Get in touch</Button> : <Text>Welcome back.</Text>}
+          {canExecute() ? (
+            <Button onClick={() => setIsOpen(true)}>Get in touch</Button>
+          ) : (
+            <Text>Welcome back.</Text>
+          )}
         </Flex>
       )}
       {isOpen && !hasSubmitted && (
-        <Form onSubmit={handleSubmit} onCancel={() => setIsOpen(false)} submitting={isSubmitting} />
+        <Form
+          onSubmit={handleSubmit}
+          onCancel={() => setIsOpen(false)}
+          submitting={isSubmitting}
+        />
       )}
       {hasSubmitted && (
         <Flex justify="center">
-          <Alert message="Message sent successfully." type="success" className="w-fit" />
+          <Alert
+            message="Message sent successfully."
+            type="success"
+            className="w-fit"
+          />
         </Flex>
       )}
     </Flex>
