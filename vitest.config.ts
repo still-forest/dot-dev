5c3adedf078f17f5ce887be20d7bbfe0e370e2f7
John Szymanowski <github@john.szymanow.ski>
2025-06-18 20:37:52 +0800

    tests running

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,26 +1,18 @@
 import { resolve } from "node:path";
-import svgr from "@svgr/rollup";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig, type ViteUserConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-      "@tests": resolve(__dirname, "./tests"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:8080",
-    },
-  },
+  plugins: [tsconfigPaths(), react()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@tests": resolve(__dirname, "./tests"),
+    },
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
