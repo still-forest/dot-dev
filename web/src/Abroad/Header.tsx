09fc6fc99f5955c66f2777680efbd1bc6da4611c
John Szymanowski <github@john.szymanow.ski>
2025-06-19 10:47:53 +0800

    front page

--- web/src/Abroad/Header.tsx
+++ web/src/Abroad/Header.tsx
@@ -1,5 +1,10 @@
 import { Heading } from "@still-forest/canopy";
+import { Link } from "react-router";
 
 export const Header = () => {
-  return <Heading level="1">Abroad</Heading>;
+  return (
+    <Link to="/abroad">
+      <Heading level="1">Abroad</Heading>
+    </Link>
+  );
 };
--- web/src/app/abroad/index.tsx
+++ web/src/app/abroad/index.tsx
@@ -1,12 +1,24 @@
-import { Container, Heading } from "@still-forest/canopy";
+import { Button, Container, Flex } from "@still-forest/canopy";
+import { Link } from "react-router";
 import { Layout } from "@/components/Layout";
 
 export const Abroad = () => {
   return (
     <Layout>
-      <Container>
-        <Heading level="1">Abroad</Heading>
-      </Container>
+      <Flex direction="col" align="center" justify="center" gap="12" className="h-full">
+        <Container>
+          <Flex justify="center" align="center">
+            <img src="/abroad/splash-icon.png" alt="Abroad logo" className="w-lg" />
+          </Flex>
+        </Container>
+        <Container>
+          <Flex justify="center" align="center">
+            <Button variant="outline" asChild>
+              <Link to="/abroad/privacy">Privacy Policy</Link>
+            </Button>
+          </Flex>
+        </Container>
+      </Flex>
     </Layout>
   );
 };
