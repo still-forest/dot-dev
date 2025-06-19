639e37c8e34f105411091249dfa14680c13ff181
John Szymanowski <github@john.szymanow.ski>
2025-06-19 18:11:42 +0800

    Add not found page

---
+++ web/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import { PageNotFound } from "@still-forest/canopy";
+import { Layout } from "@/components/Layout";
+
+export const NotFoundPage = () => {
+  return (
+    <Layout>
+      <PageNotFound />
+    </Layout>
+  );
+};
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Root } from "./app/[root]";
-import { Abroad } from "./app/abroad/index.tsx";
-import { PrivacyPage as AbroadPrivacy } from "./app/abroad/privacy.tsx";
-import ThemeProvider from "./context/ThemeProvider.tsx";
-import { THEMES } from "./context/ThemeProviderContext.tsx";
+import { Abroad } from "./app/abroad/index";
+import { PrivacyPage as AbroadPrivacy } from "./app/abroad/privacy";
+import { NotFoundPage } from "./app/not-found";
+import ThemeProvider from "./context/ThemeProvider";
+import { THEMES } from "./context/ThemeProviderContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<Root />} />
           <Route path="/abroad" element={<Abroad />} />
           <Route path="/abroad/privacy" element={<AbroadPrivacy />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
