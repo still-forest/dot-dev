239664cb7b42e4748f38f782b5841d0032218165
John Szymanowski <github@john.szymanow.ski>
2025-06-19 11:04:00 +0800

    on index too

--- web/src/app/abroad/index.tsx
+++ web/src/app/abroad/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Flex } from "@still-forest/canopy";
+import { Button, Container, Flex, Heading } from "@still-forest/canopy";
 import { Link } from "react-router";
 import { Layout } from "@/components/Layout";
 
@@ -9,6 +9,16 @@ export const Abroad = () => {
         <Container>
           <Flex justify="center" align="center">
             <img src="/abroad/splash-icon.png" alt="Abroad logo" className="w-lg" />
+          </Flex>
+        </Container>
+        <Container>
+          <Flex justify="center" align="center">
+            <Link to="/" className="flex items-center gap-2">
+              <Heading level="3" weight="thin" className="font-brand">
+                by Still Forest
+              </Heading>
+              <img src="/icon-192.png" alt="Still Forest logo" className="w-[24px]" />
+            </Link>
           </Flex>
         </Container>
         <Container>
