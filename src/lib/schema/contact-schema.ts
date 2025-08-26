90b9e9f1123d69fcc1aa1a181467458592c24f76
John Szymanowski <github@john.szymanow.ski>
2025-08-26 16:11:29 +0800

    fixes

--- src/lib/schema/contact-schema.ts
+++ src/lib/schema/contact-schema.ts
@@ -5,9 +5,11 @@ const sanitizeString = (raw: string) => {
   return validator.escape(raw.trim().replace(/<[^>]*>/g, ""));
 };
 
-export const contactSchema = z.object({
-  email: z.string().email().transform(sanitizeString),
-  message: z.string().min(10).max(1000).transform(sanitizeString),
-});
+export const contactSchema = z
+  .object({
+    email: z.string().trim().email(),
+    message: z.string().trim().min(10).max(1000).transform(sanitizeString),
+  })
+  .strict();
 
 export type ContactFormData = z.infer<typeof contactSchema>;
