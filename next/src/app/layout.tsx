b20773fcb904b7d17e83ca53bac1f74cd97095f1
John Szymanowski <github@john.szymanow.ski>
2025-08-26 08:23:17 +0800

    css/layout

--- next/src/app/layout.tsx
+++ next/src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/context/ThemeProvider";
 import { THEMES } from "@/context/ThemeProviderContext";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Still Forest",
@@ -35,7 +24,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         <ThemeProvider defaultTheme={THEMES.SYSTEM} storageKey="still-forest-theme">
           {children}
         </ThemeProvider>
