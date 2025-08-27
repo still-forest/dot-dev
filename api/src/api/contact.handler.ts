377777360953cbc8f927ab9b728e2c105ad40516
John Szymanowski <github@john.szymanow.ski>
2025-08-27 11:50:24 +0800

    Clean up contact submission (API-side) (#161)

--- api/src/api/contact.handler.ts
+++ api/src/api/contact.handler.ts
@@ -1,26 +1,23 @@
 import type { NextFunction, Request, RequestHandler, Response } from "express";
-import { isDevelopment } from "@/config";
-import { contactService } from "@/services/contact.service";
 import { getLogger } from "@/services/logger.service";
+import { contact } from "./contact.actions";
+import { ValidationError } from "./ValidationError";
 
 export const contactHandler: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const logger = getLogger("contact");
-    const { fromEmail, body } = req.body;
 
-    if (isDevelopment) {
-      logger.info("Contact form submitted in development environment", { fromEmail, body });
-      res.status(204).end();
-      return;
-    }
-
-    const [success, error] = await contactService.submitContactForm({ fromEmail, body });
+    const { success, error } = await contact(req.body);
 
     if (success) {
       res.status(204).end();
     } else {
-      logger.error("Failed to submit contact form", { error });
-      res.status(500).json({ message: "Failed to submit contact form" });
+      if (error instanceof ValidationError) {
+        res.status(400).json(error);
+      } else {
+        logger.error("Failed to submit contact form", { error });
+        res.status(500).json({ message: "Failed to submit contact form" });
+      }
     }
   } catch (error) {
     next(error);
