2be27d95ea0bedba652372734209d9e3d7c63d00
John Szymanowski <github@john.szymanow.ski>
2025-08-28 11:32:30 +0800

    don't add underline to abroad header

--- src/components/abroad/Header.tsx
+++ src/components/abroad/Header.tsx
@@ -6,7 +6,7 @@ import { AbroadLogo } from "./AbroadLogo";
 export const Header = () => {
   return (
     <Flex align="center" className="border-border border-b py-4 sm:flex-row" direction="col" gap="4" justify="between">
-      <Link href="/abroad">
+      <Link href="/abroad" noStyle>
         <Flex align="center" gap="2">
           <AbroadLogo className="w-[48px]" />
           <Heading className="font-abroad-brand text-abroad-brand" level="1">
@@ -15,7 +15,7 @@ export const Header = () => {
         </Flex>
       </Link>
       <Button asChild variant="outline">
-        <Link className="flex items-center gap-2" href="/">
+        <Link className="flex items-center gap-2" href="/" noStyle>
           <Heading className="font-brand" level="3" weight="thin">
             by Still Forest
           </Heading>
